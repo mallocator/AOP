29eec285c6d940c374ee826e51b853d0bb1d1771
Ravi Gairola <mallox@pyxzl.net>
2021-09-13 00:38:03 -0700

    Move to jest and base implementation for methods

--- src/index.js
+++ src/index.js
@@ -4,14 +4,85 @@
  */
 export default class AOP {
   /**
-   *
+   * The handler function that will either be able to modify parameters and execute code before a method is called or
+   * after where it can then modify the return value.
    * @callback Handler
-   * @param {string} name The name of the target you're wrapping
+   * @param {string} name The name of the method you're intercepting. It will come with a suffix of either ":before" or
+   *   ":after" to indicate when it was called.
    * @param {*[]} [args] An array of arguments that will be passed to the called target on 'before'
    * @param {*} [returnValue] The return value from the function that was just executed on 'after'
-   * @param {Object} context The context of in which he target is executed
    */
-  
+
+  /**
+   * This method performs the actual wrapping of methods with handlers. It does this by looking for both static and
+   * dynamic methods, which are on different properties depending on what the target is. After that it iterates over
+   * both lists and wraps each method (according to the filters) with custom function the executes the handler and
+   * allows us to modify either the method parameters or the return value.
+   * @param {Object|Class} target Can be either an object instance or a class reference
+   * @param {Handler} handler The handler function executed before the target method
+   * @param {string[]} methods A list of method name strings on the target to which this should be applied (basically a
+   *   filter). empty if should be ignored.
+   * @param {string[]} aspects Whether the execute before, after or both
+   */
+  static #wrap(target, handler, methods, aspects) {
+    let staticProps;
+    let dynamicProps;
+    if (typeof target == 'function') {
+      if (/^class/.test(target.toString())) {
+        staticProps = target;
+        dynamicProps = target.prototype;
+      } else {
+        throw new Error('Passed in single method, not yet supported but stay tuned');
+      }
+    }
+    if (typeof target == 'object') {
+      staticProps = target.constructor;
+      dynamicProps = Object.getPrototypeOf(target);
+    }
+
+    // For dynamic properties
+    for (let prop of Object.getOwnPropertyNames(dynamicProps)) {
+      const method = prop.toString();
+      // ignore constructor unless explicitly specified
+      if (method === 'constructor' || (methods.length && !methods.includes(method))) {
+        continue;
+      }
+
+      let original = dynamicProps[method];
+      dynamicProps[method] = (...args) => {
+        if (aspects.includes('before')) {
+          handler(method + ':before', args);
+        }
+        let result = original(...args);
+        if (aspects.includes('after')) {
+          // Haven't decided yet if I want to use the return value from this
+          return handler(method + ':after', result);
+        }
+        return result;
+      };
+    }
+
+    // For static properties
+    for (let prop of Object.getOwnPropertyNames(staticProps)) {
+      const method = prop.toString();
+      // ignore default properties unless explicitly specified
+      if (['length', 'prototype', 'name'].includes(method) && !methods.includes(method)) {
+        continue;
+      }
+      const original = staticProps[method];
+      staticProps[method] = (...args) => {
+        if (aspects.includes('before')) {
+          handler(method + ':before', args);
+        }
+        let result = original(...args);
+        if (aspects.includes('after')) {
+          handler(method + ':after', result);
+        }
+        return result;
+      };
+    }
+  }
+
   /**
    * Execute code before a method is called. This allows you to modify the arguments passed in on the args parameter.
    * The handler will get the list of arguments as 2nd parameter where any modification will be passed on to the
@@ -22,9 +93,9 @@ export default class AOP {
    *   (basically a filter)
    */
   static beforeMethods(target, handler, ...methods) {
-  
+    AOP.#wrap(target, handler, methods, ['before']);
   }
-  
+
   /**
    * Execute code after a method is called. This allows you to modify the return value.
    * The handler will get the return value as 2nd parameter and can decide what to return from the handler to pass on
@@ -35,53 +106,22 @@ export default class AOP {
    *   (basically a filter)
    */
   static afterMethods(target, handler, ...methods) {
-  
+    AOP.#wrap(target, handler, methods, ['after']);
   }
-  
+
   /**
-   * Execute code before an instance of a class is created. This allows you to modify the constructor arguments before
-   * they are passed on the the instance constructor. The handler wil get the constructor arguments as 2nd parameter
-   * where any modification will be passed on to the wrapped constructor.
-   * @param {Class} clazz Reference of the class we want to wrap
-   * @param {Handler} handler The handler function executed before creating the instance
+   * Execute code before and after a method is called. This allows you to modify both the parameters and the return
+   * value. The handler will in this case be called twice, once before the method is called and once after. If you want
+   * to differentiate between the two, you can look at the first parameter, the method name. The parameter will always
+   * have a suffix with either ":before" or ":after".
+   * @see beforeMethods
+   * @see afterMethods
+   * @param {Object|Class} target Can be either an object instance or a class reference
+   * @param {Handler} handler The handler function executed before the target method
+   * @param {String} [methods] An optional list of method name strings on the target to which this should be applied
+   *   (basically a filter)
    */
-  static beforeInit(clazz, handler) {
-  
-  }
-  
-  /**
-   * Execute code after an instance is created. This allows you to modify the return value.
-   * The handler will get the instance as 2nd parameter and can decide what to return from the handler to pass on
-   * to the original caller.
-   * @param {Class} clazz Reference of the class we want to wrap
-   * @param {Handler} handler The handler function executed after creating the instance
-   */
-  static afterInit(clazz, handler) {
-  
-  }
-  
-  /**
-   * Query based aspect oriented programming interface. Not yet implemented but the goal is to have something like:
-   * MyClass.set*Value
-   * (MyClass|YourClass|TheirClass).*   // Might need to parse manually
-   * My*Class.setValues?    // Might need to scan the context for available classes
-   * @param {String} query The query used to find methods on a class.
-   * @param {Handler} handler The handler function executed before the target methods
-   * @param {Object} [context] The context object in which to execute this query. Uses the global context if not
-   *   specified.
-   */
-  static injectBefore(query, handler, context) {
-  
-  }
-  
-  /**
-   * @see injectBefore
-   * @param {String} query The query used to find methods on a class.
-   * @param {Handler} handler The handler function executed after the target methods
-   * @param {Object} [context] The context object in which to execute this query. Uses the global context if not
-   *   specified.
-   */
-  static injectAfter(query, handler, context) {
-  
+  static aroundMethods(target, handler, ...methods) {
+    AOP.#wrap(target, handler, methods, ['before', 'after']);
   }
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,11 @@
-import { expect } from 'chai';
-import AOP        from '../src/index.js';
+import AOP from '../src/index.js';
+
+// TODO test primitive return value
+// TODO test with private methods - might not work if read-only
+// TODO test with native classes e.g. EventEmitter
+// TODO test around
+
+const debug = false;
 
 describe('Test base AOP functions', () => {
   /**
@@ -10,145 +16,119 @@ describe('Test base AOP functions', () => {
       this.arg1 = arg1;
       this.arg2 = arg2;
     }
-    
+
     dynamicMethod(arg1, ...arg2) {
-      return {arg1, arg2};
+      return { arg1, arg2 };
     }
-    
+
     static staticMethod(arg1, ...arg2) {
-      return {arg1, arg2};
+      return { arg1, arg2 };
     }
   }
-  
+
   it('should execute a handler before a method on an instance is called', () => {
-    const instance = new Echo(1, 2);
-    const handler = (methodName, params, instanceReference) => {
-      expect(instanceReference).to.deep.equal(instance);
-      return params[0]++;
+    const instance = new Echo();
+    const handler = (methodName, params) => {
+      debug && console.log('increasing by 1 before instance');
+      params[0]++;
     };
     AOP.beforeMethods(instance, handler);
-    
-    const result = instance.dynamicMethod(1, 2);
-    expect(result.arg1).to.equal(2);
-    expect(result.arg2).to.deep.equal([2]);
+
+    // works on dynamic methods
+    const result1 = instance.dynamicMethod(1, 2);
+    // The reason we deal with args this way is because our test class above returns an object with the params like this
+    expect(result1.arg1).toEqual(2);
+    // It depends on the method you're trying to wrap what the return value is.
+    expect(result1.arg2).toEqual([2]);
+
+    // also works on static methods of the instance
+    const result2 = Echo.staticMethod(1, 2);
+    expect(result2.arg1).toEqual(2);
+    expect(result2.arg2).toEqual([2]);
   });
-  
+
   it('should execute a handler before a method on a class is called', () => {
-    const handler = (methodName, params, instance) => {
-      expect(instance.arg1).to.equal(1);
-      expect(instance.arg2).to.deep.equal([2]);
-      return params[0]++;
+    const handler = (methodName, params) => {
+      debug && console.log('increasing by 1 before class');
+      params[0]++;
     };
     AOP.beforeMethods(Echo, handler);
-    
-    const result1 = new Echo(1, 2).dynamicMethod(1);
-    expect(result1.arg1).to.equal(2);
-    expect(result1.arg2).to.deep.equal([2]);
-    
+
+    // now increases by 2 since we already have another aspect in the chain
+    const result1 = new Echo().dynamicMethod(1, 2);
+    expect(result1.arg1).toEqual(3);
+    expect(result1.arg2).toEqual([2]);
+
+    // same with the static method
     const result2 = Echo.staticMethod(1, 2);
-    expect(result2.arg1).to.equal(2);
-    expect(result2.arg2).to.deep.equal([2]);
+    expect(result2.arg1).toEqual(3);
+    expect(result2.arg2).toEqual([2]);
   });
-  
+
   it('should execute a handler before a method on a class is called on select methods', () => {
-    const handler = (methodName, params, instance) => {
-      expect(instance.arg1).to.equal(1);
-      expect(instance.arg2).to.deep.equal([2]);
-      return params[0]++;
+    const handler = (methodName, params) => {
+      debug && console.log('increasing by 1 before class filtered');
+      params[0]++;
     };
     AOP.beforeMethods(Echo, handler, 'staticMethod');
-    
-    const result1 = new Echo(1, 2).dynamicMethod(1);
-    expect(result1.arg1).to.equal(1);
-    expect(result1.arg2).to.deep.equal([2]);
-    
+
+    // now increases by 2 since we have the other 2 from before to deal with but from this one
+    const result1 = new Echo().dynamicMethod(1, 2);
+    expect(result1.arg1).toEqual(3);
+    expect(result1.arg2).toEqual([2]);
+
+    // this one only affect the static method, hence only this one increases by yet another 1 on top of the other 2
     const result2 = Echo.staticMethod(1, 2);
-    expect(result2.arg1).to.equal(2);
-    expect(result2.arg2).to.deep.equal([2]);
+    expect(result2.arg1).toEqual(4);
+    expect(result2.arg2).toEqual([2]);
   });
-  
+
   it('should execute a handler after a method on an instance is called', () => {
-    const instance = new Echo(1, 2);
-    const handler = (methodName, returnValue, instanceReference) => {
-      expect(instanceReference).to.deep.equal(instance);
-      return returnValue++;
+    const handler = (methodName, returnValue) => {
+      debug && console.log('reducing by 1 after instance');
+      returnValue.arg1--;
+      return returnValue;
     };
+    const instance = new Echo();
     AOP.afterMethods(instance, handler);
-    
-    const result = instance.dynamicMethod(1, 2);
-    expect(result.arg1).to.equal(2);
-    expect(result.arg2).to.deep.equal([2]);
+
+    // This now increases by 2 and reduces by 1
+    const result1 = instance.dynamicMethod(1, 2);
+    expect(result1.arg1).toEqual(2);
+    expect(result1.arg2).toEqual([2]);
+
+    // Same on the static side, but here we've increased by one extra
+    const result2 = Echo.staticMethod(1, 2);
+    expect(result2.arg1).toEqual(3);
+    expect(result2.arg2).toEqual([2]);
   });
-  
+
   it('should execute a handler after a method on a class is called', () => {
-    const handler = (methodName, returnValue, instance) => {
-      expect(instance).to.deep.equal(instance);
-      return returnValue++;
+    const handler = (methodName, returnValue) => {
+      debug && console.log('reducing by 3 after class');
+      returnValue.arg1 = returnValue.arg1 - 3;
+      return returnValue;
     };
     AOP.afterMethods(Echo, handler);
-    
-    const result1 = new Echo(1, 2).dynamicMethod(1);
-    expect(result1.arg1).to.equal(2);
-    expect(result1.arg2).to.deep.equal([2]);
-    
+
+    // This should now increase by 2 and reduce by 4 total (since we reduce by 3 in this test)
+    const result1 = new Echo().dynamicMethod(1, 2);
+    expect(result1.arg1).toEqual(-1);
+    expect(result1.arg2).toEqual([2]);
+
+    // Should do the same over here, first increase by 3 and the reduce by 4 total
     const result2 = Echo.staticMethod(1, 2);
-    expect(result2.arg1).to.equal(2);
-    expect(result2.arg2).to.deep.equal([2]);
+    expect(result2.arg1).toEqual(0);
+    expect(result2.arg2).toEqual([2]);
   });
-  
-  it('should execute an aspect before an instance is created', () => {
-    const handler = (methodName, args) => {
-      return args[0]++;
-    };
-    
-    AOP.beforeInit(Echo, handler);
-    const result = new Echo(1, 2);
-    expect(result.arg1).to.equal(2);
-    expect(result.arg2).to.deep.equal([2]);
+
+  it.skip('should execute an aspect before an instance is created', () => {
+    expect.fail('This is not possible since we cannot inject code before a constructor');
   });
-  
-  it('should execute an aspect after an instance is created', () => {
-    const handler = (methodName, instance) => {
-      instance.arg1++;
-      return instance;
-    };
-    
-    AOP.afterInit(Echo, handler);
-    const result = new Echo(1, 2);
-    expect(result.arg1).to.equal(2);
-    expect(result.arg2).to.deep.equal([2]);
-  });
-  
-  it('should execute an aspect before a query for methods is used', () => {
-    const handler = (methodName, params, instance) => {
-      expect(instance.arg1).to.equal(1);
-      expect(instance.arg2).to.deep.equal([2]);
-      return params[0]++;
-    };
-    AOP.injectBefore('Echo.dynamicMethod', handler);
-    
-    const result1 = new Echo(1, 2).dynamicMethod(1);
-    expect(result1.arg1).to.equal(2);
-    expect(result1.arg2).to.deep.equal([2]);
-    
-    const result2 = Echo.staticMethod(1, 2);
-    expect(result2.arg1).to.equal(1);
-    expect(result2.arg2).to.deep.equal([2]);
-  });
-  
-  it('should execute an aspect after a a query for methods is used', () => {
-    const handler = (methodName, returnValue, instance) => {
-      expect(instance).to.deep.equal(instance);
-      return returnValue++;
-    };
-    AOP.afterMethods('Echo.staticMethod', handler);
-    
-    const result1 = new Echo(1, 2).dynamicMethod(1);
-    expect(result1.arg1).to.equal(1);
-    expect(result1.arg2).to.deep.equal([2]);
-    
-    const result2 = Echo.staticMethod(1, 2);
-    expect(result2.arg1).to.equal(2);
-    expect(result2.arg2).to.deep.equal([2]);
+
+  it.skip('should execute an aspect after an instance is created', () => {
+    expect.fail(
+      'constructors are read-only. If you had a named reference you could do it, but not via method param dynamically'
+    );
   });
 });
